b318b63ae5969bf6fded2cdf0ca662251fb1b476
akmubi <53152392+akmubi@users.noreply.github.com>
2020-08-08 02:12:08 +0500

    Update config.go

--- config.go
+++ config.go
@@ -1,68 +1,159 @@
 package scholarScraper
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"strconv"
-	"errors"
+	"strings"
+	"log"
+	"fmt"
+	"os"
+	"io"
 )
 
-type Config struct {
-	language	string
-	searchQuery	string
-	numOfPages	int64
+// Article Info ~ PDF File
+type Link struct {
+	ArticleID int64
+	Filepath string
 }
 
-const (
-	scholarDomain			= "http://scholar.google.com/scholar"
-	allowedLanguages		= "ru&en"
-	defaultLanguage			= "ru"
-	defaultNumOfPages		= 5
-)
-
-func (config *Config) New(parameters map[string]string) error {
-
-	// setting up default config parameters
-	config.numOfPages	= defaultNumOfPages
-	config.language		= defaultLanguage
-
-	if _, has_query := parameters["query"]; !has_query {
-		return errors.New("Search query is required!")
-	}
-
-	for key, value := range parameters {
-		switch key {
-		case "lang":
-				config.language = value
-		case "query":
-				config.searchQuery = value
-		case "pages":
-			numOfPages, err := strconv.ParseInt(value, 10, 64)
-			if err != nil {
-				return err
-			}
-			config.numOfPages = numOfPages
-		default:
-			return errors.New("Unknown parameter")
+func findFilenameAndNull(filepath string, links []Link) {
+	for _, link := range links {
+		if link.Filepath == filepath {
+			link.Filepath = ""
+			return
 		}
 	}
+} 
 
-	if err := config.validate(); err != nil {
-		return err
+// Serialization
+
+func DownloadPDFs(targetFolderPath string, articles []Article) ([]Link, error) {
+
+	var links []Link
+
+	err := checkAndCreateFolder(targetFolderPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, article := range articles {
+
+		var link Link
+
+		if article.PDFLink == "" {
+			fmt.Println("Article", i, " doesn't have PDF. Skipping")
+			continue
+		}
+
+		prettyName := MakeStringPretty(article.Title)
+		if prettyName == "" {
+			prettyName += strconv.FormatInt(int64(i), 10)
+			log.Println("Non printable title name!")
+		}
+		prettyName += ".pdf"
+
+
+		fmt.Println("[Downloading (" + strconv.FormatInt(int64(i + 1), 10) + 
+									"/" + strconv.FormatInt(int64(len(articles)), 10) +
+									 ")]:", prettyName)
+
+		link.ArticleID = 0
+
+		// Ask for document
+		response, err := http.Get(article.PDFLink)
+		if err != nil {
+			// do not return error
+			// because we need to download
+			// as much as possible
+			log.Println(err)
+			continue
+		}
+		defer response.Body.Close()
+
+		pdfFile, err := os.Create(targetFolderPath + prettyName)
+		if err != nil {
+			return nil, err
+		}
+
+		defer pdfFile.Close()
+
+		_, err = io.Copy(pdfFile, response.Body)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		link.Filepath = targetFolderPath + prettyName
+		links = append(links, link)
+	}
+	return links, nil
+}
+
+func checkAndCreateFolder(filepath string) error {
+	folder := getFolder(filepath)
+	if _, err := os.Stat(folder); os.IsNotExist(err) {
+		return os.Mkdir(folder, os.ModePerm)
 	}
 	return nil
 }
 
-func (config Config) validate() error {
-	if !isAllowedLanguage(allowedLanguages, config.language) {
-		return errors.New("Unknown language")
+func SaveToJSON(filepath string, object interface{}) error {
+	
+	// check if folder exists
+	err := checkAndCreateFolder(filepath)
+	if err != nil {
+		return err
 	}
 
-	// because if page count is greater than 98 a server error occurs
-	if config.numOfPages < 0 || config.numOfPages > 98 {
-		return errors.New("Incorrect page count")
+	fmt.Printf("Saving '%s'\n", filepath)
+	serialized, err := json.MarshalIndent(object, "", "\t")
+	if err != nil {
+		return err
 	}
 
-	if config.searchQuery == "" {
-		return errors.New("Seach query is empty")
+	// saving json file
+	err = ioutil.WriteFile(filepath, serialized, 0644)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func (link *Link) checkAndRemovePDF() error {
+	fmt.Printf("Checking article %d's PDF file ...\n", link.ArticleID + 1)
+	
+	// Read file
+	PDFContent, err := ioutil.ReadFile(link.Filepath)
+	if err != nil {
+		return err
+	}
+	PDFContentString := string(PDFContent)
+	
+	// if first bytes of file are %PDF then it's absolutely PDF file
+	// else we need to remove this file
+	if !strings.HasPrefix(PDFContentString, "%PDF") {
+		fmt.Printf("First 10 bytes - '%s'. Removing %s ...\n", PDFContentString[:10], link.Filepath)
+
+		err = os.Remove(link.Filepath)
+		
+		if err != nil {
+			return err
+		}
+
+		// Remove filepath from link
+		link.Filepath = ""
+	}
+	return nil
+}
+
+func RemoveFakePDFs(links []Link) error {
+	for _, link := range links {
+		err := link.checkAndRemovePDF()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }

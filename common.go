5a5dcc16d7f5e7f8ab8d8887f245b7587776510b
akmubi <53152392+akmubi@users.noreply.github.com>
2020-08-08 02:11:26 +0500

    Update common.go

--- common.go
+++ common.go
@@ -1,43 +1,29 @@
-package scholarParser
+package scholarScraper
 
 import (
-	"strconv"
 	"fmt"
 )
 
-// common interface
-type printable interface {
+type printer interface {
 	showContent()
 }
 
-func Print(object printable) {
+func Print(object printer) {
 	object.showContent()
 }
 
 func (config Config) showContent() {
 	fmt.Println("Config: {")
-	fmt.Println("\tLanguage: " + config.language + ",")
-	fmt.Println("\tNumber Of Pages: " + strconv.FormatInt(config.numOfPages, 10) + ",")
-	fmt.Println("\tSearch Query: \"" + config.searchQuery + "\",")
-	fmt.Println("\tParse Authors: " + strconv.FormatBool(config.parseAuthors) + ",")
-	fmt.Println("\tParse Descriptions: " + strconv.FormatBool(config.parseDescriptions))
+	fmt.Println("\tLanguage:"			+ config.language + ",")
+	fmt.Println("\tSearch Query: '"		+ config.searchQuery + "\",")
+	fmt.Println("\tNumber Of Pages:",	config.numOfPages, ",")
 	fmt.Println("}")
 }
 
 func (article Article) showContent() {
 	fmt.Println("Article: {")
-	fmt.Println("\tID: " + strconv.FormatInt(article.ID, 10) + ",")
-	fmt.Println("\tTitle: " + article.Title + ",")
-	fmt.Println("\tURL: " + article.URL + ",")
-	fmt.Println("\tPDF Link: " + article.PDFLink + ",")
-	fmt.Println("\tAuthors: " + article.Authors + ",")
-	fmt.Println("\tDescription: " + article.Description)
-	fmt.Println("}")
-}
-
-func (link Link) showContent() {
-	fmt.Println("Link : {")
-	fmt.Println("\tArticle ID: " + strconv.FormatInt(link.ArticleID, 10) + ",")
-	fmt.Println("\tFile Path: " + link.Filepath)
+	fmt.Println("\tTitle:" 		+ article.Title + ",")
+	fmt.Println("\tHTML Link:"	+ article.HTMLLink + ",")
+	fmt.Println("\tPDF Link:"	+ article.PDFLink + ",")
 	fmt.Println("}")
 }
